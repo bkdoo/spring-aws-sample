01040e857f72f8ae1bb47fd0abfc034b67e6c0d7
BKDOO <kyeongdoos@gmail.com>
2022-05-04 17:48:05 +0900

    API 생성 및 테스트

---
+++ src/main/java/com/bkdoo/spring_aws/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.bkdoo.spring_aws.service.posts;
+
+import com.bkdoo.spring_aws.domain.posts.PostsRepository;
+import com.bkdoo.spring_aws.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/bkdoo/spring_aws/web/PostApiController.java
@@ -0,0 +1,21 @@
+package com.bkdoo.spring_aws.web;
+
+
+import com.bkdoo.spring_aws.service.posts.PostsService;
+import com.bkdoo.spring_aws.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
